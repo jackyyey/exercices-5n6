71458c29a96046ce2509fcdbc01d73af165672af
Jacky Ye <6272880@cegepmontpetit.ca>
2025-10-07 15:30:15 -0400

    formatif partie 1

---
+++ exercices/nom_pages/lib/main.dart
@@ -0,0 +1,127 @@
+import 'package:flutter/material.dart';
+import 'package:nom_pages/detail.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        // This is the theme of your application.
+        //
+        // TRY THIS: Try running your application with "flutter run". You'll see
+        // the application has a purple toolbar. Then, without quitting the app,
+        // try changing the seedColor in the colorScheme below to Colors.green
+        // and then invoke "hot reload" (save your changes or press the "hot
+        // reload" button in a Flutter-supported IDE, or press "r" if you used
+        // the command line to start the app).
+        //
+        // Notice that the counter didn't reset back to zero; the application
+        // state is not lost during the reload. To reset the state, use hot
+        // restart instead.
+        //
+        // This works for code too, not just values: Most code changes can be
+        // tested with just a hot reload.
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+      ),
+      home: const MyHomePage(title: 'Accueil'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+
+  // This widget is the home page of your application. It is stateful, meaning
+  // that it has a State object (defined below) that contains fields that affect
+  // how it looks.
+
+  // This class is the configuration for the state. It holds the values (in this
+  // case the title) provided by the parent (in this case the App widget) and
+  // used by the build method of the State. Fields in a Widget subclass are
+  // always marked "final".
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  int _counter = 0;
+
+  void _incrementCounter() {
+    setState(() {
+      // This call to setState tells the Flutter framework that something has
+      // changed in this State, which causes it to rerun the build method below
+      // so that the display can reflect the updated values. If we changed
+      // _counter without calling setState(), then the build method would not be
+      // called again, and so nothing would appear to happen.
+      _counter++;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    // This method is rerun every time setState is called, for instance as done
+    // by the _incrementCounter method above.
+    //
+    // The Flutter framework has been optimized to make rerunning build methods
+    // fast, so that you can just rebuild anything that needs updating rather
+    // than having to individually change instances of widgets.
+    return Scaffold(
+      appBar: AppBar(
+        // TRY THIS: Try changing the color here to a specific color (to
+        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
+        // change color while the other colors stay the same.
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        // Here we take the value from the MyHomePage object that was created by
+        // the App.build method, and use it to set our appbar title.
+        title: Text(widget.title),
+      ),
+      drawer: Drawer(
+        child: ListView(
+          padding: EdgeInsets.zero,
+          children: [
+            const DrawerHeader(child: Text("mon super tiroir bleu!|!!!!!"), decoration: BoxDecoration(color: Colors.blue),),
+            ListTile(
+              title: Text("Détails"),
+              leading: Icon(Icons.abc, color: Colors.black,),
+              onTap: (){ Navigator.of(context).push(MaterialPageRoute(builder: (context)=> Details())); }
+            )
+          ],
+        ),
+      ),
+      body: Center(
+        // Center is a layout widget. It takes a single child and positions it
+        // in the middle of the parent.
+        child: Column(
+          children:[
+            Container(height: 100, child:
+              Row(
+                children: [
+                  Expanded(flex: 1 ,child: Container(color: Colors.blue, child: Column(),)),
+                  Expanded(flex: 2 ,child: Container(color: Colors.red, child: Column(),))
+                ],
+              )
+            ),
+            Expanded(flex: 1, child: Container(child: Row(children: [],), color: Colors.green,) ),
+            Expanded(flex: 1, child: Padding(padding: EdgeInsets.all(8), child: Container(child: Row(children: [],), color: Colors.yellow,),)  ),
+            Expanded(child: Row(children: [
+              Expanded(flex: 1 ,child: Container(child: Column(), color: Colors.orange,)),
+              Expanded(flex: 2 ,child: Column()),
+              Expanded(flex: 1 ,child: Container(child: Column(), color: Colors.pink,) )
+            ],))
+          ]
+        ),
+      ),
+    );
+  }
+}
