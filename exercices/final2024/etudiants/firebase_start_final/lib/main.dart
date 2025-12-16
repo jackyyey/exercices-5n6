b77919ff4d87e11d60104edda4bae76791e0ab2b
Jacky Ye <6272880@cegepmontpetit.ca>
2025-12-16 12:39:48 -0500

    final partie 1

--- exercices/final2024/etudiants/firebase_start_final/lib/main.dart
+++ exercices/final2024/etudiants/firebase_start_final/lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'firebase_options.dart';
 import 'package:flutter/material.dart';
@@ -34,9 +35,39 @@ class MyHomePage extends StatefulWidget {
 
   @override
   State<MyHomePage> createState() => _MyHomePageState();
+
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    Firebase.initializeApp();
+  }
+
+  var doc;
+  var etudiant;
+
+
+  void getData() async {
+    DocumentReference colref = FirebaseFirestore.instance.collection('etudiants').doc("6272880");
+    doc = await colref.get();
+    etudiant = doc.data();
+    print(etudiant);
+    setState(() {
+
+    });
+  }
+
+  void mettreAJour() async {
+    DocumentReference colref = FirebaseFirestore.instance.collection('etudiants').doc("6272880");
+    colref.update({"complete": "true"});
+    setState(() {
+
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -45,7 +76,35 @@ class _MyHomePageState extends State<MyHomePage> {
         title: Text(widget.title),
       ),
       body: Center(
-        child: Text("Allo"),
+        child:
+        Column(children: [
+          Expanded(flex: 1 ,child: Row(children: [
+            if (etudiant != null) ...[
+              Column(children: [
+                Row(children: [
+                  Text("Nom: ${etudiant["nom"]}"),
+                ],),
+                Row(children: [
+                  Text("prenom: ${etudiant["prenom"]}"),
+                ],),
+                Row(children: [
+                  Text("complete: ${etudiant["complete"]}"),
+                ],),
+
+              ],)
+            ] else ...[
+              Text("Aucun étudiant chargé"),
+            ]
+          ],
+          )),
+          Expanded(flex: 1, child: Row( mainAxisAlignment: MainAxisAlignment.center ,children: [
+            ElevatedButton(onPressed: getData, child: Text("Get Data")),
+            ElevatedButton(onPressed: mettreAJour, child: Text("update") ),
+          ],),),
+
+
+
+        ],),
       ),
     );
   }
